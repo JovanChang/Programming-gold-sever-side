230400c1e3cea1ddcdbf1ac4a3422f6d1e160d2b
JovanChang <jovanchang19@gmail.com>
2023-02-16 23:27:45 +0000

    inintal commit

---
+++ sever.js
@@ -0,0 +1,11 @@
+//express template for routing
+const express = require('express')
+const app = express()
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
